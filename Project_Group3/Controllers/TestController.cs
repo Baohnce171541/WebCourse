9ffdab4e7c09f54eba6b88e93739c76e89344e60
huynhbao <huynhnguyenbao3105@gmail.com>
2024-03-20 21:27:42 +0700

    test git

--- Project_Group3/Controllers/TestController.cs
+++ Project_Group3/Controllers/TestController.cs
@@ -5,17 +5,89 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using WebLibrary.DAO;
+using WebLibrary.Models;
+using WebLibrary.Repository;
 
 namespace Project_Group3.Controllers
 {
-  
+
     public class TestController : Controller
     {
-        public IActionResult  hahahahaha()
+
+        IVoucherRepository voucherRepository = null;
+
+        public TestController()
         {
-         System.Console.WriteLine("ghahahahahahahahahahahahahahahah");
-            return View();
+            voucherRepository = new VoucherRepository();
         }
-        
+
+        public IActionResult Index()
+        {
+            var list = voucherRepository.VouchersList();
+            return View(list);
+        }
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(Voucher voucher)
+        {
+            try
+            {
+                if (voucher == null)
+                {
+                    ViewBag.err = "voucher cannot be null. Please try again.";
+                    return View();
+                }
+
+                if (ModelState.IsValid)
+                {
+                    var generatedCode = voucherRepository.GenerateRandomCode();
+                    voucher = new Voucher
+                    {
+                        CodeVoucher = generatedCode,
+                        AdminId = 1,
+                        PercentDiscount = voucher.PercentDiscount,
+                        EndAt = voucher.EndAt,
+                        StartAt = voucher.StartAt,
+                        IsActive = voucher.IsActive
+                    };
+                    string voucherInfo = $"Voucher ID: {voucher.VoucherId}\n" +
+                                         $"Code Voucher: {voucher.CodeVoucher}\n" +
+                                         $"Admin ID: {voucher.AdminId}\n" +
+                                         $"Percent Discount: {voucher.PercentDiscount}\n" +
+                                         $"End Date: {voucher.EndAt}\n" +
+                                         $"Start Date: {voucher.StartAt}\n" +
+                                         $"Is Active: {voucher.IsActive}";
+                    Console.WriteLine(voucherInfo);
+                    voucherRepository.CreateVoucher(voucher);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    var errorProperties = ModelState.Keys.Where(k => ModelState[k].Errors.Any()).ToList();
+                    ViewBag.ErrorProperties = errorProperties;
+                    return View(voucher);
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return View(voucher);
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Voucher voucher = voucherRepository.GetVoucherByID(id);
+
+            if (voucher != null)
+            {
+                VoucherDAO.Instance.Remove(voucher.VoucherId);
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
