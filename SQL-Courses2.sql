aeaa0c06f69591c7e2df08704e0c4718c7420713
HongLinh <linhthce172074@fpt.edu.vn>
2024-03-21 14:23:38 +0700

    Sql

--- SQL-Courses2.sql
+++ SQL-Courses2.sql
@@ -492,23 +492,7 @@ BEGIN
     DELETE FROM instructor WHERE instructorID IN (SELECT instructorID FROM deleted);
 END;
 GO
-
 ---
-CREATE TRIGGER delete_learner_trigger
-ON learner
-INSTEAD OF DELETE
-AS
-BEGIN
-    -- Xóa các bài đăng ký (enrollment) của người học (learner) bị xóa
-    DELETE FROM enrollment WHERE learnerID IN (SELECT learnerID FROM deleted);
-
-    -- Xóa các đánh giá (review) của người học (learner) bị xóa
-    DELETE FROM review WHERE learnerID IN (SELECT learnerID FROM deleted);
-
-    -- Xóa người học (learner) bị xóa
-    DELETE FROM learner WHERE learnerID IN (SELECT learnerID FROM deleted);
-END;
-
 CREATE TRIGGER delete_course_trigger
 ON courses
 INSTEAD OF DELETE
@@ -544,6 +528,32 @@ BEGIN
 	   DELETE FROM quiz WHERE chapterID IN (SELECT chapterID FROM deleted);
     -- Xóa các chương (chapters) thuộc về khóa học (course) bị xóa
     DELETE FROM chapter WHERE chapterID IN (SELECT chapterID FROM deleted);
+END;
+GO
+---
+CREATE TRIGGER delete_learner_trigger
+ON learner
+INSTEAD OF DELETE
+AS
+BEGIN
+    -- Xóa các bài đăng ký (enrollment) của người học (learner) bị xóa
+    DELETE FROM enrollment WHERE learnerID IN (SELECT learnerID FROM deleted);
+
+    -- Xóa các đánh giá (review) của người học (learner) bị xóa
+    DELETE FROM review WHERE learnerID IN (SELECT learnerID FROM deleted);
+
+    -- Xóa người học (learner) bị xóa
+    DELETE FROM learner WHERE learnerID IN (SELECT learnerID FROM deleted);
+END;
+GO
+CREATE TRIGGER delete_answer_trigger
+ON answer
+INSTEAD OF DELETE
+AS
+BEGIN
+    DELETE FROM quiz
+    WHERE answerID IN (SELECT answerID FROM deleted);
+	  DELETE FROM answer WHERE answerID IN (SELECT answerID FROM deleted);
 END;
 GO
 
